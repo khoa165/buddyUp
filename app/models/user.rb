61e0781d581928cf36fab69043b79eb028562ac0
Khoa Le <khoatle165@gmail.com>
2019-08-17 20:58:19 +0200

    allow user to upload photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :photo, PhotoUploader
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
