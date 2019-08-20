052cb5d62bc6fd8fc5853eb010a16bad3a278dad
Khoa Le <khoatle165@gmail.com>
2019-08-20 12:20:24 +0200

    association between user and other models

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,6 +3,6 @@ class Connection < ApplicationRecord
   belongs_to :user
   has_many :meetings, dependent: :destroy
   has_many :conversations, dependent: :destroy
-  has_many :messages, through: :conversations, dependent: :destroy
+  has_many :messages, through: :conversations
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :connections, dependent: :destroy
+  has_many :messages
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
