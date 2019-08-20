afa1b10b396cad976802218d5113e00c6322834c
Khoa Le <khoatle165@gmail.com>
2019-08-20 18:24:55 +0200

    personality, lifestyle, music, movie, sport, technical seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Question.destroy_all
+Response.destroy_all
+
+puts "Creating questions"
+
+puts "-------------------------------------"
+
+puts "---> Personality question"
+personality_question = Question.create(description: "Personality", is_required: true)
+personalities = ["Active", "Calm", "Caring", "Cheerful", "Cooperative", "Creative", "Determined", "Easygoing", "Empathetic", "Enthusiastic", "Flexible", "Friendly", "Fun", "Honest", "Joker", "Optimistic", "Organized", "Passionate", "Practical", "Proactive", "Rational", "Relaxed", "Sociable", "Tolerant"]
+personalities.each do |personality|
+  Response.create(answer: personality, question: personality_question)
+end
+puts "---> Personalities: #{personalities.size}"
+
+puts "-------------------------------------"
+
+puts "---> Lifestyle question"
+lifestyle_question = Question.create(description: "Lifestyle", is_required: true)
+lifestyles = ["Adventurer", "Art enthusiast", "Athlete", "Avid music lover", "Book lover", "Chef", "Cinema goer", "Culture enthusiast", "Dancer", "Early bird", "Entrepreneur", "Family person", "Fashionista", "Foodie", "Gamer", "Health enthusiast", "Homebird", "Night owl", "Partier", "Pet lover", "Photographer", "Shopper", "Singer", "Tech lover", "Theater fan", "Traveller", "Vegan", "Vegetarian", "Walker", "Workaholic"]
+lifestyles.each do |lifestyle|
+  Response.create(answer: lifestyle, question: lifestyle_question)
+end
+puts "---> Lifestyles: #{lifestyles.size}"
+
+puts "-------------------------------------"
+
+puts "---> Music question"
+music_question = Question.create(description: "Music")
+musics = ["Alternative", "Asian pop", "Blues", "Classical", "Country", "Dance", "Electronic", "Electronic", "Folk", "Funk", "Gospel", "Hip-Hop", "Indie", "Instrumental", "Jazz", "Latin", "Lounge", "Metal", "New age", "Opera", "Pop", "Punk", "R&B", "Reggae", "Rock", "Ska", "Soul"]
+musics.each do |music|
+  Response.create(answer: music, question: music_question)
+end
+puts "---> Musics: #{musics.size}"
+
+puts "-------------------------------------"
+
+puts "---> Sport question"
+sport_question = Question.create(description: "Sport")
+sports = ["American football", "Athletics", "Badminton", "Baseball", "Basketball", "Bodyboarding", "Bowling", "Bowls", "Boxing", "Canoeing", "Climbing", "Cricket", "Diving", "Football", "Go-karting", "Golf", "Gym", "Hiking", "Hockey", "Horse riding", "Martial arts", "Motorcycling", "Padel", "Pole dancing", "Rowing", "Rugby", "Running", "Skateboarding", "Skating", "Skiing", "Snowboarding", "Squash", "Surfing", "Swimming", "Table tennis", "Tennis", "Volleyball", "Water polo", "Windsurfing"]
+sports.each do |sport|
+  Response.create(answer: sport, question: sport_question)
+end
+puts "---> Sports: #{sports.size}"
+
+puts "-------------------------------------"
+
+puts "---> Movie question"
+movie_question = Question.create(description: "Movie")
+movies = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Historical", "Horror", "Mystery", "Political", "Romance", "Saga", "Satire", "Sci-fi", "Thriller", "Western"]
+movies.each do |movie|
+  Response.create(answer: movie, question: movie_question)
+end
+puts "---> Movies: #{movies.size}"
+
+puts "-------------------------------------"
+
+puts "---> Technical question"
+movie_question = Question.create(description: "Movie")
+movies = ["Programming", "Web development", "Full-stack", "Front-end", "Back-end", "Data science", "Machine learning", "Artificial intelligent (AI)", "Freelancing"]
+movies.each do |movie|
+  Response.create(answer: movie, question: movie_question)
+end
+puts "---> Movies: #{movies.size}"
+
+puts "-------------------------------------"
+
+puts "Finished!"
