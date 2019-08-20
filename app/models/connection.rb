a2f510d4830caf3968ab3f4413b0f21a905d5f6b
Khoa Le <khoatle165@gmail.com>
2019-08-20 12:09:51 +0200

    associations for connection

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,8 @@
 class Connection < ApplicationRecord
+  # Associations
   belongs_to :user
-  belongs_to :user
+  has_many :meetings, dependent: :destroy
+  has_many :conversations, dependent: :destroy
+  has_many :messages, through: :conversations, dependent: :destroy
+
 end
