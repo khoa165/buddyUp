d397031ad5d6bc5b8a10f053fcfa52578dfce834
Arman Balani <arubalani@gmail.com>
2019-08-20 18:29:59 +0200

    added controller create action and views for user responses

--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -1,4 +1,13 @@
 class UserResponsesController < ApplicationController
   def create
+    user_responses_params[:response_ids].each do |response_id|
+      UserResponse.create(response_id: response_id, user: current_user)
+    end
+  end
+
+  private
+
+  def user_responses_params
+    params.require(:user_responses).permit(response_ids: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
       resources :user_responses, only: [:create]
     end
   end
+  resources :user_responses, only: [:create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
