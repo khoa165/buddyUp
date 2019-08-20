7d4379a878f8a0f12f68d8b29bc00a2aed28c01d
danko beribak <beribakdanko@gmail.com>
2019-08-20 17:45:04 +0200

    connection controller private functions partially done

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,9 +1,8 @@
 class ConnectionsController < ApplicationController
   def index
-
     users = User.geocoded
     users = users.where.not(user: current_user)
-    @matches = compute_score(current_user, users)
+    @matches = retrieve_buddies(users)
 
     # species_query = params[:species]
     # if species_query.present?
@@ -17,7 +16,15 @@ class ConnectionsController < ApplicationController
       params.require(:connection).permit()
   end
 
-  def compute_score(current_user, matches)
+  def retrieve_buddies(users)
+    options = []
+    users.each do |user|
+      options << [user, compute_score(current_user, user)]
+    end
+    options.sort_by { |option| -option[1] }
+  end
+
+  def compute_score(current_user, target_user)
 
   end
 end
