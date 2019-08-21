b3c3253ee4e0fcf44acd09dbd4c065f9554f6675
Khoa Le <khoatle165@gmail.com>
2019-08-21 19:15:12 +0200

    finish computing score

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,23 +1,24 @@
 class ConnectionsController < ApplicationController
   def index
-    users = User.geocoded
-    users = users.where.not(user: current_user)
+    # users = User.geocoded
+    # users = users.where.not(user: current_user)
 
-    query = params[:location]
-    if query.present?
-      # users = users.where("species ILIKE ?", "%#{query}%")
-      users = users.near(query, 20)
-    else
-      users = users.near([current_user.latitude, current_user.longitude], 20)
-    end
+    # query = params[:location]
+    # if query.present?
+    #   # users = users.where("species ILIKE ?", "%#{query}%")
+    #   users = users.near(query, 20)
+    # else
+    #   users = users.near([current_user.latitude, current_user.longitude], 20)
+    # end
 
+    users = User.where.not(user: current_user)
     @matches = retrieve_buddies(users)
   end
 
   private
 
   def retrieve_buddies(users)
-    current_user_data = retrieve_current_user_data
+    current_user_data = retrieve_all_data(current_user)
     weights = compute_weight(current_user_data)
     options = []
     users.each do |user|
@@ -26,11 +27,11 @@ class ConnectionsController < ApplicationController
     options.sort_by { |option| -option[1] }
   end
 
-  def retrieve_current_user_data
-    current_user_data = current_user.user_responses.includes(:response).group_by {
+  def retrieve_all_data(user)
+    user_data = user.user_responses.includes(:response).group_by {
       |ur| ur.response.question_id
     }
-    return current_user_data
+    return user_data
   end
 
   def compute_weight(current_user_data)
@@ -42,11 +43,24 @@ class ConnectionsController < ApplicationController
   end
 
   def compute_total_score(current_user_data, weights, target_user)
-    target_data = target_user.user_responses.includes(:response).group_by {
-      |ur| ur.response.question_id
-    }
-    current_user_data.each_value do |responses_chosen|
-
+    raw_score = []
+    target_data = retrieve_all_data(target_user)
+    current_user_data.each do |key, value|
+      raw_score << compute_component_score(value, target_data(key))
     end
+    total_score = 0
+    raw_score.each_with_index do |component_score, index|
+      total_score += component_score * weights[index]
+    end
+    return total_score.round
+  end
+
+  def compute_component_score(current_user_component_data, target_component_data)
+    score = 0
+    unless current_user_component_data.nil? || target_component_data.nil?
+      overlap_component_data = current_user_component_data & target_component_data
+      score = overlap_component_data.size.to_f / current_user_component_data.size
+    end
+    return score
   end
 end
