690a75d5204e9a16c629d41817950066b2b3387b
Arman Balani <arubalani@gmail.com>
2019-08-21 16:35:09 +0200

    added the dropdown and highlighting features in javascript

--- app/assets/javascripts/response.js
+++ app/assets/javascripts/response.js
@@ -5,8 +5,8 @@ const responses = document.querySelectorAll(".js-response");
 // console.log(responses);
 
 const addCheckedStatus = (event) => {
-  event.target.classList.toggle('btn-outline-primary')
-  event.target.classList.toggle('btn-primary')
+  event.target.classList.toggle('btn-outline-primary');
+  event.target.classList.toggle('btn-primary');
 }
 
 
@@ -14,6 +14,17 @@ responses.forEach(response => {
   response.addEventListener("click", addCheckedStatus);
 })
 
+const responsesText = document.querySelectorAll(".responses");
+const questions = document.querySelectorAll(".question");
+const toggleResponsesText = (event) => {
+  if (event.target.nextSibling.nextSibling.classList.contains("responses")) {
+    event.target.nextSibling.nextSibling.classList.toggle('hide');
+  }
+}
+
+questions.forEach(question => {
+  addEventListener("click", toggleResponsesText);
+})
 
 // label.addEventListener("click", (event) => {
 //   console.log(event);
