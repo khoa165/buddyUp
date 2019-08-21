378b657f9c29f777dbdc807284983537ef990a6e
Arman Balani <arubalani@gmail.com>
2019-08-21 18:04:48 +0200

    added drop down arrow to the profile selection

--- app/assets/javascripts/response.js
+++ app/assets/javascripts/response.js
@@ -8,18 +8,20 @@ const addCheckedStatus = (event) => {
   event.target.classList.toggle('btn-outline-primary');
   event.target.classList.toggle('btn-primary');
 }
-
-
 responses.forEach(response => {
   response.addEventListener("click", addCheckedStatus);
 })
-
 const responsesText = document.querySelectorAll(".responses");
 const questions = document.querySelectorAll(".question");
 const toggleResponsesText = (event) => {
-  if (event.target.nextSibling.nextSibling.classList.contains("responses")) {
+  if (event.target.classList.contains("question") && event.target.nextSibling.nextSibling.classList.contains("responses")) {
     event.target.nextSibling.nextSibling.classList.toggle('hide');
-  }
+    event.target.firstElementChild.classList.toggle('fa-chevron-down')
+    event.target.firstElementChild.classList.toggle('fa-chevron-up') }
+  else if (event.target.classList.contains("fas")) {
+    event.target.parentElement.nextElementSibling.classList.toggle('hide')
+    event.target.classList.toggle('fa-chevron-down')
+    event.target.classList.toggle('fa-chevron-up') }
 }
 
 questions.forEach(question => {
