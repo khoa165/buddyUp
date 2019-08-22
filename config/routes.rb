caa4e0ced11d7d957f4b2cc79c9376ba906969d3
Khoa Le <khoatle165@gmail.com>
2019-08-22 14:29:12 +0200

    comment for resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,31 @@
 Rails.application.routes.draw do
+  # Default pages provided from the devise.
   devise_for :users
+  # Set root for the website.
   root to: 'pages#home'
-
+  # Questions.
   resources :questions, only: [:index]
-
+  # Reponses.
   resources :responses, only: [] do
+    # Nested create of user responses
     resources :user_responses, only: [:create]
   end
-
+  # User responses.
   resources :user_responses, only: :destroy
-
+  # Connections.
   resources :connections, only: [:index, :create] do
+    # Custom route of search
     collection do
       get 'search'
     end
-    resources :messages, only: [:index]
+    # Nested create of user responses
+    resources :messages, only: [:index, :create]
   end
+  # Dashboard.
+  resources :dashboard, only: [:index, :show, :edit, :update]
 
-  get '/profile/show', to: 'dashboard#show'
-  get '/profile/edit', to: 'dashboard#edit'
-  patch '/profile', to: 'dashboard#update'
-  resources :connections, only: [:index]
-  # resources :dashboard, only: [:edit, :update, :show, :index]
+  # get '/profile/show', to: 'dashboard#show'
+  # get '/profile/edit', to: 'dashboard#edit'
+  # patch '/profile', to: 'dashboard#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
