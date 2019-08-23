fb6f3009c93c92b44018f7aac7993c8bbea85a33
Khoa Le <khoatle165@gmail.com>
2019-08-23 14:06:54 +0200

    add route for connection show

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # User responses.
   resources :user_responses, only: :destroy
   # Connections.
-  resources :connections, only: [:index, :create] do
+  resources :connections, only: [:index, :create, :show] do
     # Custom route of search
     collection do
       get 'search'
