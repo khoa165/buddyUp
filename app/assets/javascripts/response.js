d0cde592218715a27af3e6b13c2921908bf31efc
Arman Balani <arubalani@gmail.com>
2019-08-23 13:30:02 +0200

    last minute changes on preference page

--- app/assets/javascripts/response.js
+++ app/assets/javascripts/response.js
@@ -12,19 +12,19 @@ responses.forEach(response => {
   response.addEventListener("click", addCheckedStatus);
 })
 const responsesText = document.querySelectorAll(".responses");
-const questions = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".questions");
 const toggleResponsesText = (event) => {
-  if (event.target.classList.contains("questions") && event.target.nextSibling.nextSibling.classList.contains("responses")) {
-    event.target.nextSibling.nextSibling.classList.toggle('hide');
-    event.target.firstElementChild.classList.toggle('fa-chevron-down')
-    event.target.firstElementChild.classList.toggle('fa-chevron-up') }
-  else if (event.target.classList.contains("fas")) {
-    event.target.parentElement.nextElementSibling.classList.toggle('hide')
-    event.target.classList.toggle('fa-chevron-down')
-    event.target.classList.toggle('fa-chevron-up') }
+  console.log(event)
+  if (event.target.parentElement.classList.contains("questions")) {
+    event.target.parentElement.nextElementSibling.classList.toggle('hide');
+    event.target.parentElement.lastElementChild.classList.toggle('fa-chevron-down');
+    event.target.parentElement.lastElementChild.classList.toggle('fa-chevron-up')
+  }
+  console.log(event)
 }
 
 questions.forEach(question => {
+  console.log(event)
   addEventListener("click", toggleResponsesText);
 })
 
@@ -33,3 +33,7 @@ questions.forEach(question => {
 //   event.classList.remove('btn-outline-primary');
 //   event.classList.add('btn-primary');
 // });
+// else if (event.target.classList.contains("fas")) {
+  //   event.target.parentElement.nextElementSibling.classList.toggle('hide')
+  //   event.target.classList.toggle('fa-chevron-down')
+  //   event.target.classList.toggle('fa-chevron-up') }
