8225a58cce2bc81e8f196df1d275477063f97fef
Khoa Le <khoatle165@gmail.com>
2019-08-27 06:13:15 +0200

    try to fix chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,7 @@ class Message < ApplicationRecord
     ActionCable.server.broadcast("connection_#{connection.id}", {
       message_partial: ApplicationController.renderer.render(
         partial: "messages/arriving_message",
+        # partial: "messages/show",
         locals: { message: self, user_is_messages_author: false }
       ),
       current_user_id: user.id
