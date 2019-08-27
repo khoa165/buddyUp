275da1f62a7d982fb6e66ff4682b8829744de5e3
Khoa Le <khoatle165@gmail.com>
2019-08-27 11:14:58 +0200

    reorganize js code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { delegator } from '../functions/photoUploadDelegator';
+import '../functions/scrollToLastMessage';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { loadDynamicBannerText } from '../components/banner';
 import { dynamicResponses } from '../components/response';
