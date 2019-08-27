e961b02ff2689bce35bd79e76e7a223ead469562
Arman Balani <arubalani@gmail.com>
2019-08-27 11:10:44 +0200

    small change to profile page

--- app/javascript/components/response.js
+++ app/javascript/components/response.js
@@ -14,15 +14,22 @@ responses.forEach(response => {
   response.addEventListener("click", addCheckedStatus);
 })
 const responsesText = document.querySelectorAll(".responses");
+console.log(responsesText)
+const toggleOtherResponses = (responsesText) => {
+  responsesText.forEach(response => {
+    if (response.classList.contains("hide") == false) {
+      response.classList.toggle("hide");
+    }
+  })
+}
+
 const questions = document.querySelectorAll(".questions");
 const toggleResponsesText = (event) => {
-  console.log(event)
   if (event.target.parentElement.classList.contains("questions")) {
     event.target.parentElement.nextElementSibling.classList.toggle('hide');
     event.target.parentElement.lastElementChild.classList.toggle('fa-chevron-down');
     event.target.parentElement.lastElementChild.classList.toggle('fa-chevron-up')
   }
-  console.log(event)
 }
 
 questions.forEach(question => {
