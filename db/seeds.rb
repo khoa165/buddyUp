4176bdda733370fc7666f1aa9b2a4a0ca38a9e3a
Khoa Le <khoatle165@gmail.com>
2019-08-28 18:47:13 +0200

    modify to have smarter seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ puts "---------------------------------------------------"
 
 puts "---> Music question"
 music_question = Question.create(description: "Music")
-musics = ["Alternative", "Asian pop", "Blues", "Classical", "Country", "Dance", "Electronic", "Electronic", "Folk", "Funk", "Gospel", "Hip-Hop", "Indie", "Instrumental", "Jazz", "Latin", "Lounge", "Metal", "New age", "Opera", "Pop", "Punk", "R&B", "Reggae", "Rock", "Ska", "Soul"]
+musics = ["Alternative", "Asian pop", "Blues", "Classical", "Country", "Dance", "Electronic", "Folk", "Funk", "Gospel", "Hip-Hop", "Indie", "Instrumental", "Jazz", "Latin", "Lounge", "Metal", "New age", "Opera", "Pop", "Punk", "R&B", "Reggae", "Rock", "Ska", "Soul"]
 musics.each do |music|
   Response.create(answer: music, question: music_question)
 end
@@ -136,29 +136,29 @@ puts "Creating classmates..."
 
 puts "---------------------------------------------------"
 
-abs = User.create!(intro: "amazing coding bootcamp student", first_name: "Abs", last_name: "Yade", email: "abs@gmail.com", password: "abs123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars0.githubusercontent.com/u/49921457?v=4")
-amine = User.create!(intro: "amazing coding bootcamp student", first_name: "Amine", last_name: "Boukhriss", email: "amine@gmail.com", password: "amine123", age: 25, gender: "Male", address: "Frankfurt", country: "Spain", city: "Frankfurt", remote_photo_url: "https://avatars3.githubusercontent.com/u/28905895?v=4")
-barbara = User.create!(intro: "member of the cool car", first_name: "Barbara", last_name: "Peric", email: "barbara@gmail.com", password: "barbara123", age: 25, gender: "Female", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars3.githubusercontent.com/u/50910606?v=4")
-cihad = User.create!(intro: "member of the cool car", first_name: "Cihad", last_name: "Saglam", email: "cihad@gmail.com", password: "cihad123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars0.githubusercontent.com/u/29354309?v=4")
-fahad = User.create!(intro: "amazing coding bootcamp student", first_name: "Fahad", last_name: "Al Rasheed", email: "fahad@gmail.com", password: "fahad123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/yli3bpplfnizttpmou1w.jpg")
-ferdinand = User.create!(intro: "amazing coding bootcamp student", first_name: "Ferdinand", last_name: "Croy", email: "ferdinand@gmail.com", password: "ferdinand123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/j1kt7zpoasrxus5aym8i.jpg")
-francois = User.create!(intro: "amazing coding bootcamp student", first_name: "Francois", last_name: "Adrien", email: "francois@gmail.com", password: "francois123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars3.githubusercontent.com/u/52235257?v=4")
-glen = User.create!(intro: "amazing coding bootcamp student", first_name: "Glen", last_name: "Warren", email: "glen@gmail.com", password: "glen123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars3.githubusercontent.com/u/44340845?v=4")
-jaime = User.create!(intro: "amazing coding bootcamp student", first_name: "Jaime", last_name: "Cruz", email: "jaime@gmail.com", password: "jaime123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars0.githubusercontent.com/u/39839800?v=4")
-josephine = User.create!(intro: "amazing coding bootcamp student", first_name: "Josephine", last_name: "Geoghegan", email: "josephine@gmail.com", password: "josephine123", age: 25, gender: "Female", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars2.githubusercontent.com/u/48995429?v=4")
-kaveh = User.create!(intro: "amazing coding bootcamp student", first_name: "Kaveh", last_name: "Jalilian", email: "kaveh@gmail.com", password: "kaveh123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/wnf628rqyln9aciahxcp.jpg")
-maria = User.create!(intro: "amazing coding bootcamp student", first_name: "Maria", last_name: "Manninen", email: "maria@gmail.com", password: "maria123", age: 25, gender: "Female", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars1.githubusercontent.com/u/50732002?v=4")
-matthis = User.create!(intro: "amazing coding bootcamp student", first_name: "Matthis", last_name: "Parisot", email: "matthis@gmail.com", password: "matthis123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars1.githubusercontent.com/u/52250484?v=4")
-mehdi = User.create!(intro: "amazing coding bootcamp student", first_name: "Mehdi", last_name: "Idrissi Kaitouni", email: "mehdi@gmail.com", password: "mehdi123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/wi1aedmjvhykylpeywj1.jpg")
-nicolas = User.create!(intro: "member of the cool car", first_name: "Nicolas", last_name: "Proto", email: "nicolas@gmail.com", password: "nicolas123", age: 25, gender: "Male", address: "Frankfurt", country: "Spain", city: "Frankfurt", remote_photo_url: "https://avatars0.githubusercontent.com/u/43278026?v=4")
-nikita = User.create!(intro: "amazing coding bootcamp student", first_name: "Nikita", last_name: "Bandarevich", email: "nikita@gmail.com", password: "nikita123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars2.githubusercontent.com/u/34006151?v=4")
-sy = User.create!(intro: "amazing coding bootcamp student", first_name: "Sy", last_name: "Rashid", email: "sy@gmail.com", password: "sy1234", age: 25, gender: "Male", address: "Frankfurt", country: "Spain", city: "Frankfurt", remote_photo_url: "https://avatars2.githubusercontent.com/u/6656014?v=4")
-thomas = User.create!(intro: "amazing coding bootcamp student", first_name: "Thomas", last_name: "Bonnet", email: "thomas@gmail.com", password: "thomas123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars3.githubusercontent.com/u/49944627?v=4")
-uri = User.create!(intro: "amazing coding bootcamp student", first_name: "Uri", last_name: "Serra", email: "uri@gmail.com", password: "uri123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars1.githubusercontent.com/u/50989100?v=4")
-victor = User.create!(intro: "amazing coding bootcamp student", first_name: "Victor", last_name: "Padilla", email: "victor@gmail.com", password: "victor123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars1.githubusercontent.com/u/51762154?v=4")
-wendela = User.create!(intro: "member of the cool car", first_name: "Wendela", last_name: "Lovgren", email: "wendela@gmail.com", password: "wendela123", age: 25, gender: "Female", address: "Frankfurt", country: "Spain", city: "Frankfurt", remote_photo_url: "https://avatars1.githubusercontent.com/u/48654392?v=4")
-xinyi = User.create!(intro: "amazing coding bootcamp student", first_name: "Xinyi", last_name: "Wu", email: "xinyi@gmail.com", password: "xinyi123", age: 25, gender: "Female", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars2.githubusercontent.com/u/51039948?v=4")
-zach = User.create!(intro: "amazing coding bootcamp student", first_name: "Zach", last_name: "Alvstad", email: "zach@gmail.com", password: "zach123", age: 25, gender: "Male", address: ["Barcelona", "Madrid"].sample, country: "Spain", city: ["Barcelona", "Madrid"].sample, remote_photo_url: "https://avatars3.githubusercontent.com/u/52245667?v=4")
+abs = User.create!(intro: "amazing coding bootcamp student", first_name: "Abs", last_name: "Yade", email: "abs@gmail.com", password: "abs123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars0.githubusercontent.com/u/49921457?v=4")
+amine = User.create!(intro: "amazing coding bootcamp student", first_name: "Amine", last_name: "Boukhriss", email: "amine@gmail.com", password: "amine123", age: 25, gender: "Male", address: "Frankfurt", country: "Germany", city: "Frankfurt", remote_photo_url: "https://avatars3.githubusercontent.com/u/28905895?v=4")
+barbara = User.create!(intro: "member of the cool car", first_name: "Barbara", last_name: "Peric", email: "barbara@gmail.com", password: "barbara123", age: 25, gender: "Female", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars3.githubusercontent.com/u/50910606?v=4")
+cihad = User.create!(intro: "member of the cool car", first_name: "Cihad", last_name: "Saglam", email: "cihad@gmail.com", password: "cihad123", age: 25, gender: "Male", address: "Madrid", country: "Spain", city: "Madrid", remote_photo_url: "https://avatars0.githubusercontent.com/u/29354309?v=4")
+fahad = User.create!(intro: "amazing coding bootcamp student", first_name: "Fahad", last_name: "Al Rasheed", email: "fahad@gmail.com", password: "fahad123", age: 25, gender: "Male", address: "Madrid", country: "Spain", city: "Madrid", remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/yli3bpplfnizttpmou1w.jpg")
+ferdinand = User.create!(intro: "amazing coding bootcamp student", first_name: "Ferdinand", last_name: "Croy", email: "ferdinand@gmail.com", password: "ferdinand123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/j1kt7zpoasrxus5aym8i.jpg")
+francois = User.create!(intro: "amazing coding bootcamp student", first_name: "Francois", last_name: "Adrien", email: "francois@gmail.com", password: "francois123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars3.githubusercontent.com/u/52235257?v=4")
+glen = User.create!(intro: "amazing coding bootcamp student", first_name: "Glen", last_name: "Warren", email: "glen@gmail.com", password: "glen123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars3.githubusercontent.com/u/44340845?v=4")
+jaime = User.create!(intro: "amazing coding bootcamp student", first_name: "Jaime", last_name: "Cruz", email: "jaime@gmail.com", password: "jaime123", age: 25, gender: "Male", address: "Madrid", country: "Spain", city: "Madrid", remote_photo_url: "https://avatars0.githubusercontent.com/u/39839800?v=4")
+josephine = User.create!(intro: "amazing coding bootcamp student", first_name: "Josephine", last_name: "Geoghegan", email: "josephine@gmail.com", password: "josephine123", age: 25, gender: "Female", address: "Madrid", country: "Spain", city: "Madrid", remote_photo_url: "https://avatars2.githubusercontent.com/u/48995429?v=4")
+kaveh = User.create!(intro: "amazing coding bootcamp student", first_name: "Kaveh", last_name: "Jalilian", email: "kaveh@gmail.com", password: "kaveh123", age: 25, gender: "Male", address: "Madrid", country: "Spain", city: "Madrid", remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/wnf628rqyln9aciahxcp.jpg")
+maria = User.create!(intro: "amazing coding bootcamp student", first_name: "Maria", last_name: "Manninen", email: "maria@gmail.com", password: "maria123", age: 25, gender: "Female", address: "Valencia", country: "Spain", city: "Valencia", remote_photo_url: "https://avatars1.githubusercontent.com/u/50732002?v=4")
+matthis = User.create!(intro: "amazing coding bootcamp student", first_name: "Matthis", last_name: "Parisot", email: "matthis@gmail.com", password: "matthis123", age: 25, gender: "Male", address: "Valencia", country: "Spain", city: "Valencia", remote_photo_url: "https://avatars1.githubusercontent.com/u/52250484?v=4")
+mehdi = User.create!(intro: "amazing coding bootcamp student", first_name: "Mehdi", last_name: "Idrissi Kaitouni", email: "mehdi@gmail.com", password: "mehdi123", age: 25, gender: "Male", address: "Valencia", country: "Spain", city: "Valencia", remote_photo_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/wi1aedmjvhykylpeywj1.jpg")
+nicolas = User.create!(intro: "member of the cool car", first_name: "Nicolas", last_name: "Proto", email: "nicolas@gmail.com", password: "nicolas123", age: 25, gender: "Male", address: "Frankfurt", country: "Germany", city: "Frankfurt", remote_photo_url: "https://avatars0.githubusercontent.com/u/43278026?v=4")
+nikita = User.create!(intro: "amazing coding bootcamp student", first_name: "Nikita", last_name: "Bandarevich", email: "nikita@gmail.com", password: "nikita123", age: 25, gender: "Male", address: "Valencia", country: "Spain", city: "Valencia", remote_photo_url: "https://avatars2.githubusercontent.com/u/34006151?v=4")
+sy = User.create!(intro: "amazing coding bootcamp student", first_name: "Sy", last_name: "Rashid", email: "sy@gmail.com", password: "sy1234", age: 25, gender: "Male", address: "Frankfurt", country: "Germany", city: "Frankfurt", remote_photo_url: "https://avatars2.githubusercontent.com/u/6656014?v=4")
+thomas = User.create!(intro: "amazing coding bootcamp student", first_name: "Thomas", last_name: "Bonnet", email: "thomas@gmail.com", password: "thomas123", age: 25, gender: "Male", address: "Valencia", country: "Spain", city: "Valencia", remote_photo_url: "https://avatars3.githubusercontent.com/u/49944627?v=4")
+uri = User.create!(intro: "amazing coding bootcamp student", first_name: "Uri", last_name: "Serra", email: "uri@gmail.com", password: "uri123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars1.githubusercontent.com/u/50989100?v=4")
+victor = User.create!(intro: "amazing coding bootcamp student", first_name: "Victor", last_name: "Padilla", email: "victor@gmail.com", password: "victor123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars1.githubusercontent.com/u/51762154?v=4")
+wendela = User.create!(intro: "member of the cool car", first_name: "Wendela", last_name: "Lovgren", email: "wendela@gmail.com", password: "wendela123", age: 25, gender: "Female", address: "Frankfurt", country: "Germany", city: "Frankfurt", remote_photo_url: "https://avatars1.githubusercontent.com/u/48654392?v=4")
+xinyi = User.create!(intro: "amazing coding bootcamp student", first_name: "Xinyi", last_name: "Wu", email: "xinyi@gmail.com", password: "xinyi123", age: 25, gender: "Female", address: "Frankfurt", country: "Germany", city: "Frankfurt", remote_photo_url: "https://avatars2.githubusercontent.com/u/51039948?v=4")
+zach = User.create!(intro: "amazing coding bootcamp student", first_name: "Zach", last_name: "Alvstad", email: "zach@gmail.com", password: "zach123", age: 25, gender: "Male", address: "Barcelona", country: "Spain", city: "Barcelona", remote_photo_url: "https://avatars3.githubusercontent.com/u/52245667?v=4")
 
 puts "Creating random profiles..."
 
@@ -223,17 +223,87 @@ puts "---------------------------------------------------"
 
 categories = [music_question, sport_question, movie_question, technical_question, entrepreneur_question, show_question]
 
+# User.all.each do |user|
+#   personality_question.responses.sample(rand(3..5)).each do |ur|
+#     UserResponse.create(user: user, response: ur)
+#   end
+#   lifestyle_question.responses.sample(rand(3..5)).each do |ur|
+#     UserResponse.create(user: user, response: ur)
+#   end
+#   categories.each do |category|
+#     category.responses.sample(rand(0..5)).each do |ur|
+#       UserResponse.create(user: user, response: ur)
+#     end
+#   end
+# end
+
 User.all.each do |user|
-  personality_question.responses.sample(rand(3..5)).each do |ur|
-    UserResponse.create(user: user, response: ur)
+  # Music
+  # Pop, R&B
+  # Random between Asian pop, Indie
+  # Random between Classical, Folk
+  [74, 76, [56, 66].sample, [58, 62].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
   end
-  lifestyle_question.responses.sample(rand(3..5)).each do |ur|
-    UserResponse.create(user: user, response: ur)
+  # Sports
+  # Badminton, Surfing
+  # Random between Baseball, Football
+  # Random between Golf, Swimming
+  [83, 113, [84, 94].sample, [96, 114].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
   end
-  categories.each do |category|
-    category.responses.sample(rand(0..5)).each do |ur|
-      UserResponse.create(user: user, response: ur)
-    end
+  # Movies
+  # Comedy
+  # Random between Animation, Thriller
+  # Random between Horror, Mystery
+  [123, [122, 136].sample, [129, 130].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+  # TV show
+  # The Big Bang Theory
+  # Random between Survivor, Big Brother
+  [177, [183, 184].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+end
+
+# Custom UserResponse of Personality and Lifestyle for user from each city.
+User.where(address: "Barcelona").each do |user|
+  # Cheerful, Creative, Enthusiastic
+  # Random between Calm, Caring, Relaxed, Sociable
+  [4, 6, 10, [2, 3, 22, 23].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+  # Adventurer, Art enthusiast, Pet lover
+  # Random between Athlete, Culture Enthusiast, Family person, Traveller
+  [25, 26, 44, [27, 32, 36, 50].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+end
+
+User.where(address: "Madrid").each do |user|
+  # Active, Determined, Friendly
+  # Random between Fun, Optimistic, Organized, Passionate
+  [1, 7, 12, [13, 16, 17, 18].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+  # Adventurer, Art enthusiast, Pet lover
+  # Random between Athlete, Culture Enthusiast, Family person, Traveller
+  [25, 26, 44, [27, 32, 36, 50].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+end
+
+User.where(address: "Frankfurt").each do |user|
+  # Enthusiastic, Organized, Relaxed
+  # Random between Easygoing, Empathetic, Practical, Proactive
+  [10, 17, 22, [8, 9, 19, 20].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
+  end
+  # Adventurer, Art enthusiast, Pet lover
+  # Random between Athlete, Culture Enthusiast, Family person, Traveller
+  [25, 26, 44, [27, 32, 36, 50].sample].each do |r|
+    UserResponse.create(user: user, response_id: r)
   end
 end
 
