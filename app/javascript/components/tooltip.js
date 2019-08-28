9f92bf87679b3b7493ca0fffbb26c2f189726650
Khoa Le <khoatle165@gmail.com>
2019-08-28 14:09:02 +0200

    add warning for cancel session of result page

--- app/javascript/components/tooltip.js
+++ app/javascript/components/tooltip.js
@@ -1,6 +1,8 @@
 const initTooltip = () => {
   $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip({placement: 'bottom',trigger: 'manual'}).tooltip('show');
+    // $('[data-toggle="tooltip"]').tooltip({placement: 'bottom',trigger: 'manual'}).tooltip('show');
+    // $('.custom-bu-tooltip').tooltip({placement: 'top',trigger: 'manual'}).tooltip('show');
+    $('.custom-bu-tooltip').tooltip({placement: 'top',trigger: 'manual'}).tooltip('show');
   });
 };
 
